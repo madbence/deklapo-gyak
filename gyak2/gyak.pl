dca397ddbffa8a4b871d0baf50682a2274b83e12
Dányi Bence <madbence@gmail.com>
2013-09-29 14:43:34 +0200

    Moar Prolog :3
    (\ /)
    (^.^)
    (> <) Bunny happy, good change

--- gyak2/gyak.pl
+++ gyak2/gyak.pl
@@ -51,3 +51,65 @@ fa_levelerteke2(T, L) :-  fa_reszfaja(T, leaf(L)).
 lista_prefixuma([H|_], [H]).
 lista_prefixuma([H|T], [H|T2]) :- lista_prefixuma(T, T2).
 
+%+1
+fa_melysege(leaf(_),0).
+fa_melysege(node(C1, C2), D) :-
+  fa_melysege(C1, D1),
+  fa_melysege(C2, D2),
+  D is max(D1, D2)+1.
+
+%+2
+fa_balerteke(leaf(L), L).
+fa_balerteke(node(C, _), L) :-
+  fa_balerteke(C, L).
+
+%+3
+fa_jobberteke(leaf(L), L).
+fa_jobberteke(node(_, C), L) :-
+  fa_jobberteke(C, L).
+
+%+4.
+fa_rendezett(leaf(_)).
+fa_rendezett(node(C1, C2)) :-
+  fa_rendezett(C1),
+  fa_rendezett(C2),
+  fa_balerteke(C1, L1),
+  fa_balerteke(C2, L2),
+  fa_jobberteke(C1, R1),
+  fa_jobberteke(C2, R2),
+  L1 =< R1,
+  R1 < L2,
+  L2 =< R2.
+
+%+5.
+fa_tukorkepe(leaf(L), leaf(L)).
+fa_tukorkepe(node(C1, C2), node(C3, C4)) :- fa_tukorkepe(C1, C4), fa_tukorkepe(C2, C3).
+
+%+6.
+fa_tukros(T) :- fa_tukorkepe(T, T).
+
+%+7.
+prefix_length(_, [], 0).
+prefix_length([H|T], [H|P], X) :-
+  X > 0,
+  X1 is X-1,
+  prefix_length(T, P, X1).
+
+%+8.
+suffix_before(L, L, 0).
+suffix_before([_|T], L, X) :-
+  X>0,
+  X1 is X-1,
+  suffix_before(T, L, X1).
+
+%+9.
+sublist(L, P, B, Len) :-
+  suffix_before(L, P1, B),
+  prefix_length(P1, P, Len).
+
+%+10.
+lnko(A, 0, A).
+lnko(A, B, L) :-
+  B > 0,
+  A1 is A mod B,
+  lnko(B, A1, L).
